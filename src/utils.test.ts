26d6e9159f08d7ede0325d75ffb15f2a9d990da8
Katarina Grujic <katarina.grujic97@gmail.com>
2020-11-21 00:26:31 +0100

    utilizes pipeWith

--- src/utils.test.ts
+++ src/utils.test.ts
@@ -2,11 +2,12 @@ import { validateJmbg } from "./utils";
 
 describe("validate JMBG", () => {
   test("length", () => {
-      expect(validateJmbg('1')).toBeInstanceOf(Error);
-      expect(validateJmbg('1234567890123')).toBe(null);
+    expect(validateJmbg("1")).toBeInstanceOf(Error);
+    const input = "1234567890123";
+    expect(validateJmbg(input)).toBe(input);
   });
   test("days", () => {
-      expect(validateJmbg('3234567890123')).toBeInstanceOf(Error);
-      expect(validateJmbg('0034567890123')).toBeInstanceOf(Error);
+    expect(validateJmbg("3234567890123")).toBeInstanceOf(Error);
+    expect(validateJmbg("0034567890123")).toBeInstanceOf(Error);
   });
 });
--- src/utils.ts
+++ src/utils.ts
@@ -1,15 +1,24 @@
+import { pipeWith } from "ramda";
 import { Person } from "./models";
 
-export const validateJmbg = (value: Person["jmbg"]): Error | null => {
-  if (/^\d{13}$/.test(value)) {
-    if (/^((0[1-9])|(1[0-9])|(2[0-9])|(3[0-1]))/.test(value)) {
-      return null;
-    } else {
-      return new Error(
-        "First two digits must represent a valid day in the month."
-      );
-    }
-  } else {
-    return new Error("JMBG must consist of 13 digits.");
-  }
+type MaybeJmbg = Person["jmbg"] | Error;
+
+const pipe = pipeWith(((
+  f: (value: Person["jmbg"]) => MaybeJmbg,
+  res: MaybeJmbg
+) => (res instanceof Error ? res : f(res))) as Parameters<typeof pipeWith>[0]);
+
+export const validateJmbg = (jmbg: Person["jmbg"]): MaybeJmbg => {
+  return pipe([
+    (value) =>
+      /^\d{13}$/.test(value as Person["jmbg"])
+        ? value
+        : new Error("JMBG must consist of 13 digits."),
+    (value: Person["jmbg"]) =>
+      /^((0[1-9])|(1[0-9])|(2[0-9])|(3[0-1]))/.test(value)
+        ? value
+        : new Error(
+            "First two digits must represent a valid day in the month."
+          ),
+  ])(jmbg);
 };
