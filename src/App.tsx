544ccad8d3f25078b008bf04ba111b85f5653cd3
Katarina Grujic <katarina.grujic97@gmail.com>
2020-11-20 21:22:47 +0100

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { hot } from 'react-hot-loader';
+
+function App() {
+  return (
+    <div>
+      Hello JMBG
+    </div>
+  );
+}
+
+export default hot(module)(App);
