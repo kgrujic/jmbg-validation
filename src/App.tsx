76fb9212bf857a2430e645638cdcd016e03f10fa
Katarina Grujic <katarina.grujic97@gmail.com>
2020-11-20 22:02:26 +0100

    adds form

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,50 @@
+import { useFormik } from 'formik';
 import React from 'react';
 import { hot } from 'react-hot-loader';
+import * as yup from 'yup';
+
+interface Person {
+  firstName: string
+  lastName: string
+  jmbg: string
+}
+
+const initialValues: Person = {
+  firstName: '',
+  lastName: '',
+  jmbg: '',
+} 
+
+const validationSchema = yup.object().required().shape<Person>({
+  firstName: yup.string().required(),
+  lastName: yup.string().required(),
+  jmbg: yup.string().required(),
+})
 
 function App() {
+  const form  = useFormik(React.useMemo(() => ({
+    initialValues,
+    validationSchema,
+    onSubmit: console.log,
+  }), []))
+  
   return (
     <div>
-      Hello JMBG
+      <form onSubmit={form.handleSubmit}>
+        <label >
+          Name
+          <input {...form.getFieldProps('name')} placeholder="Jane" />
+        </label>
+        <label >
+          Last Name
+          <input {...form.getFieldProps('lastName')} placeholder="Doe" />
+        </label>
+        <label >
+          JMBG
+          <input {...form.getFieldProps('jmbg')} placeholder="2008998715084" />
+        </label>
+        <button type="submit" onClick={form.submitForm}>Submit</button>
+      </form>
     </div>
   );
 }
