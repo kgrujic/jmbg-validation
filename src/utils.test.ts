4863ad715b175dc508fd5bb0c08243b4094151ba
Katarina Grujic <katarina.grujic97@gmail.com>
2020-11-28 23:56:35 +0100

    adds years and removes moths

--- src/utils.test.ts
+++ src/utils.test.ts
@@ -3,13 +3,10 @@ import { validateJmbg } from "./utils";
 describe("validate JMBG", () => {
   test("length", () => {
     expect(validateJmbg("1")).toBeInstanceOf(Error);
-    const input = "1234567890123";
-    expect(validateJmbg(input)).toBeInstanceOf(Error);
-    expect(validateJmbg("320569874521")).toBeInstanceOf(Error);
+    expect(validateJmbg("012345678912")).toBeInstanceOf(Error);
   });
-  test("months", () => {
-    expect(validateJmbg("0434567890123")).toBeInstanceOf(Error);
-    expect(validateJmbg("1500567890123")).toBeInstanceOf(Error);
-    expect(validateJmbg("3002567890123")).toBeInstanceOf(Error);
+  test("years", () => {
+    expect(validateJmbg("2902001890123")).toBeInstanceOf(Error);
+    expect(typeof validateJmbg("2902996890123")).toBe("string");
   });
 });
--- src/utils.ts
+++ src/utils.ts
@@ -13,9 +13,13 @@ const pipe = pipeWith(
     res instanceof Error ? res : f(res)
 );
 
+const m28 = "(0[1-9]|1[0-9]|2[0-8])";
 const m29 = "(0[1-9]|1[0-9]|2[0-9])";
 const m30 = "(0[1-9]|1[0-9]|2[0-9]|30)";
 const m31 = "(0[1-9]|1[0-9]|2[0-9]|3[0-1])";
+
+const months = `((${m31}01)|(${m28}02)|(${m31}03)|(${m30}04)|(${m31}05)|(${m30}06)|(${m31}07)|(${m31}08)|(${m30}09)|(${m31}10)|(${m30}11)|(${m31}12))`;
+const leapYearMonths = `((${m31}01)|(${m29}02)|(${m31}03)|(${m30}04)|(${m31}05)|(${m30}06)|(${m31}07)|(${m31}08)|(${m30}09)|(${m31}10)|(${m30}11)|(${m31}12))`;
 
 export const validateJmbg = (jmbg: Person["jmbg"]): MaybeJmbg => {
   return pipe([
@@ -25,7 +29,7 @@ export const validateJmbg = (jmbg: Person["jmbg"]): MaybeJmbg => {
         : new Error("JMBG must consist of 13 digits."),
     (value: Person["jmbg"]) =>
       new RegExp(
-        `^((${m31}01)|(${m29}02)|(${m31}03)|(${m30}04)|(${m31}05)|(${m30}06)|(${m31}07)|(${m31}08)|(${m30}09)|(${m31}10)|(${m30}11)|(${m31}12))`
+        `^((${leapYearMonths}[0-9][13579][26])|(${leapYearMonths}[0-9][02468][048])|(${months}[0-9][13579][1345789])|(${months}[0-9][02468][1235679]))`
       ).test(value)
         ? value
         : new Error(
