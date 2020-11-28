de87425e3460a96f7d79de487686e604072926ea
Katarina Grujic <katarina.grujic97@gmail.com>
2020-11-29 00:13:07 +0100

    adds region

--- src/utils.test.ts
+++ src/utils.test.ts
@@ -9,4 +9,11 @@ describe("validate JMBG", () => {
     expect(validateJmbg("2902001890123")).toBeInstanceOf(Error);
     expect(typeof validateJmbg("2902996890123")).toBe("string");
   });
+  test("region", () => {
+    expect(validateJmbg("2902001000123")).toBeInstanceOf(Error);
+    expect(typeof validateJmbg("2902996010123")).toBe("string");
+    expect(validateJmbg("2902996690123")).toBeInstanceOf(Error);
+    expect(validateJmbg("2902996600123")).toBeInstanceOf(Error);
+    expect(typeof validateJmbg("2902996700123")).toBe("string");
+  });
 });
--- src/utils.ts
+++ src/utils.ts
@@ -21,6 +21,8 @@ const m31 = "(0[1-9]|1[0-9]|2[0-9]|3[0-1])";
 const months = `((${m31}01)|(${m28}02)|(${m31}03)|(${m30}04)|(${m31}05)|(${m30}06)|(${m31}07)|(${m31}08)|(${m30}09)|(${m31}10)|(${m30}11)|(${m31}12))`;
 const leapYearMonths = `((${m31}01)|(${m29}02)|(${m31}03)|(${m30}04)|(${m31}05)|(${m30}06)|(${m31}07)|(${m31}08)|(${m30}09)|(${m31}10)|(${m30}11)|(${m31}12))`;
 
+const years = `((${leapYearMonths}[0-9][13579][26])|(${leapYearMonths}[0-9][02468][048])|(${months}[0-9][13579][1345789])|(${months}[0-9][02468][1235679]))`;
+
 export const validateJmbg = (jmbg: Person["jmbg"]): MaybeJmbg => {
   return pipe([
     (value) =>
@@ -28,9 +30,7 @@ export const validateJmbg = (jmbg: Person["jmbg"]): MaybeJmbg => {
         ? value
         : new Error("JMBG must consist of 13 digits."),
     (value) =>
-      new RegExp(
-        `^((${leapYearMonths}[0-9][13579][26])|(${leapYearMonths}[0-9][02468][048])|(${months}[0-9][13579][1345789])|(${months}[0-9][02468][1235679]))`
-      ).test(value)
+      new RegExp(`^(${years}((0[1-9])|([2-5][0-9])|([7-9][0-9])))`).test(value)
         ? value
         : new Error(
             "First two digits must represent a valid day in the month."
