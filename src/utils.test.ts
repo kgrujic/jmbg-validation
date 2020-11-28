2c1f3e572bd3d2143a586e2fd7ab35891bf744a8
Katarina Grujic <katarina.grujic97@gmail.com>
2020-11-28 23:18:35 +0100

    adds months

--- src/utils.test.ts
+++ src/utils.test.ts
@@ -4,10 +4,18 @@ describe("validate JMBG", () => {
   test("length", () => {
     expect(validateJmbg("1")).toBeInstanceOf(Error);
     const input = "1234567890123";
-    expect(validateJmbg(input)).toBe(input);
+    expect(validateJmbg(input)).toBeInstanceOf(Error);
+    expect(validateJmbg("320569874521")).toBeInstanceOf(Error);
   });
   test("days", () => {
     expect(validateJmbg("3234567890123")).toBeInstanceOf(Error);
     expect(validateJmbg("0034567890123")).toBeInstanceOf(Error);
+    const input = "1203356789012";
+    expect(validateJmbg(input)).toBe(input);
+  });
+  test("months", () => {
+    expect(validateJmbg("0434567890123")).toBeInstanceOf(Error);
+    expect(validateJmbg("1500567890123")).toBeInstanceOf(Error);
+    expect(validateJmbg("3002567890123")).toBeInstanceOf(Error);
   });
 });
--- src/utils.ts
+++ src/utils.ts
@@ -13,6 +13,10 @@ const pipe = pipeWith(
     res instanceof Error ? res : f(res)
 );
 
+const m29 = "(0[1-9]|1[0-9]|2[0-9])";
+const m30 = "(0[1-9]|1[0-9]|2[0-9]|30)";
+const m31 = "(0[1-9]|1[0-9]|2[0-9]|3[0-1])";
+
 export const validateJmbg = (jmbg: Person["jmbg"]): MaybeJmbg => {
   return pipe([
     (value) =>
@@ -21,6 +25,14 @@ export const validateJmbg = (jmbg: Person["jmbg"]): MaybeJmbg => {
         : new Error("JMBG must consist of 13 digits."),
     (value: Person["jmbg"]) =>
       /^((0[1-9])|(1[0-9])|(2[0-9])|(3[0-1]))/.test(value)
+        ? value
+        : new Error(
+            "First two digits must represent a valid day in the month."
+          ),
+    (value: Person["jmbg"]) =>
+      new RegExp(
+        `^((${m31}01)|(${m29}02)|(${m31}03)|(${m30}04)|(${m31}05)|(${m30}06)|(${m31}07)|(${m31}08)|(${m30}09)|(${m31}10)|(${m30}11)|(${m31}12))`
+      ).test(value)
         ? value
         : new Error(
             "First two digits must represent a valid day in the month."
