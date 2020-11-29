146a381319de8933a2255ccc9aa9e35249958e72
Katarina Grujic <katarina.grujic97@gmail.com>
2020-11-29 01:09:56 +0100

    declares constants before usage

--- src/utils.ts
+++ src/utils.ts
@@ -11,6 +11,20 @@ const pipeWithError = pipeWith(
     res instanceof Error ? res : f(res)
 );
 
+const d28 = "(0[1-9]|1[0-9]|2[0-8])";
+const d29 = "(0[1-9]|1[0-9]|2[0-9])";
+const d30 = "(0[1-9]|1[0-9]|2[0-9]|30)";
+const d31 = "(0[1-9]|1[0-9]|2[0-9]|3[0-1])";
+
+const months = `((${d31}01)|(${d28}02)|(${d31}03)|(${d30}04)|(${d31}05)|(${d30}06)|(${d31}07)|(${d31}08)|(${d30}09)|(${d31}10)|(${d30}11)|(${d31}12))`;
+const leapYearMonths = `((${d31}01)|(${d29}02)|(${d31}03)|(${d30}04)|(${d31}05)|(${d30}06)|(${d31}07)|(${d31}08)|(${d30}09)|(${d31}10)|(${d30}11)|(${d31}12))`;
+
+const years = `((${leapYearMonths}[0-9][13579][26])|(${leapYearMonths}[0-9][02468][048])|(${months}[0-9][13579][1345789])|(${months}[0-9][02468][1235679]))`;
+
+const region = "((0[1-9])|([2-5][0-9])|([7-9][0-9]))";
+
+const index = "[0-9][0-9][0-9]";
+
 const hasValidLength = (value: Person["jmbg"]) =>
   /^\d{13}$/.test(value) ? value : new Error("JMBG must consist of 13 digits.");
 
@@ -40,20 +54,6 @@ const hasValidControlNumber = (value: Person["jmbg"]) => {
         `Expected control number to be ${controlNumber} but received ${m}.`
       );
 };
-
-const d28 = "(0[1-9]|1[0-9]|2[0-8])";
-const d29 = "(0[1-9]|1[0-9]|2[0-9])";
-const d30 = "(0[1-9]|1[0-9]|2[0-9]|30)";
-const d31 = "(0[1-9]|1[0-9]|2[0-9]|3[0-1])";
-
-const months = `((${d31}01)|(${d28}02)|(${d31}03)|(${d30}04)|(${d31}05)|(${d30}06)|(${d31}07)|(${d31}08)|(${d30}09)|(${d31}10)|(${d30}11)|(${d31}12))`;
-const leapYearMonths = `((${d31}01)|(${d29}02)|(${d31}03)|(${d30}04)|(${d31}05)|(${d30}06)|(${d31}07)|(${d31}08)|(${d30}09)|(${d31}10)|(${d30}11)|(${d31}12))`;
-
-const years = `((${leapYearMonths}[0-9][13579][26])|(${leapYearMonths}[0-9][02468][048])|(${months}[0-9][13579][1345789])|(${months}[0-9][02468][1235679]))`;
-
-const region = "((0[1-9])|([2-5][0-9])|([7-9][0-9]))";
-
-const index = "[0-9][0-9][0-9]";
 
 export const validateJmbg = pipeWithError([
   hasValidLength,
